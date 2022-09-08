fb057370dd23d3ac97a75db505fd72b88c635170
preston <preston.ng@keplergrp.com>
2022-09-08 13:45:11 +0100

    adjective CRUD works

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:banana@localhost:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Adjective(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,15 +39,23 @@ class Verb(db.Model):
     def __repr__(self):
         return f'<Adjective {self.word}>'
 
+class WordSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'word')
+
+word_schema = WordSchema()
+words_schema = WordSchema(many=True)
+
 @app.route("/adjective", methods=["GET"])
 def get_adjectives():
-    result = Adjective.Query.all()
-    return jsonify(result.data)
+    all_adjectives = Adjective.query.all()
+    result = words_schema.dump(all_adjectives)
+    return jsonify(result)
 
 @app.route("/adjective/<id>", methods=["GET"])
 def get_adjective(id):
     adjective = Adjective.query.get(id)
-    return jsonify(adjective)
+    return word_schema.jsonify(adjective)
 
 @app.route("/adjective", methods=["POST"])
 def add_adjective():
@@ -54,21 +63,21 @@ def add_adjective():
     new_adjective = Adjective(word = new_word)
     db.session.add(new_adjective)
     db.session.commit()
-    return jsonify(new_adjective)
+    return word_schema.jsonify(new_adjective)
 
 @app.route("/adjective/<id>", methods=["POST"])
-def adjective_update(id):
+def update_adjective(id):
     adjective = Adjective.query.get(id)
     new_word = request.json["word"]
     adjective.word = new_word
     db.session.commit()
-    return jsonify(adjective)
+    return word_schema.jsonify(adjective)
 
 @app.route("/adjective/<id>", methods=["DELETE"])
-def adjective_delete(id):
+def delete_adjective(id):
     adjective = Adjective.query.get(id)
     db.session.delete(adjective)
     db.session.commit()
-    return jsonify(adjective)
+    return word_schema.jsonify(adjective)
 
 
