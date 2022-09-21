d0aa8a515fbdcc4e3780506e9b1e7b2eaec62600
preston <preston.ng@keplergrp.com>
2022-09-21 15:21:49 +0100

    reorganized files

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:banana@localhost:5432/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://madlibs_dev:madlibs_dev@localhost:5432/madlibs_dev'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from .routes import routes
@@ -14,9 +14,11 @@ def create_app():
 
 def register_extensions(app: Flask) -> None:
     from app import models
-    from app.extensions import db, migrate, ma
+    from app.extensions import db, migrate
 
     db.init_app(app)
+    with app.app.context():
+        db.create_all()
     migrate.init_app(app, db=db)
-    kip_auth.init_app(app)
+
 
--- server/app/extensions.py
+++ server/app/extensions.py
@@ -1,6 +1,7 @@
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app import app
 
 db = SQLAlchemy()
 ma = Marshmallow()
