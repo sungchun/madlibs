f11eb63a94a07d9d635d1c693fee661d8d244dff
preston <preston.ng@keplergrp.com>
2022-09-22 15:58:02 +0100

    can seed db

--- server/app/models/Adverb.py
+++ server/app/models/Adverb.py
@@ -3,6 +3,7 @@ from app.extensions import db
 class Adverb(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(100), nullable=False, unique=True)
+    schema = 'WordSchema'
 
     def __repr__(self):
         return f'Adverb {self.word}>'
--- server/app/populate_tables.py
+++ server/app/populate_tables.py
@@ -1,39 +1,49 @@
-import app
-from app.extensions import db
-from app.models import Adjective, Adverb, Noun, Verb
+from extensions import db
+from models import Adjective, Adverb, Noun, Verb
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import csv
+
+engine = create_engine('postgresql://madlibs_dev:madlibs_dev@db/madlibs_dev')
+
+Session = sessionmaker()
+local_session = Session(bind=engine)
 
 def populate_adjectives():
     with open("/home/projuser/src/app/words/adjectives.csv", "r") as file:
         rows = csv.reader(file)
         for row in rows:
             new = Adjective.Adjective(word=row)
-            db.session.add(new)
-        db.session.commit()
+            local_session.add(new)
+
+        local_session.commit()
 
 def populate_adverbs():
     with open("/home/projuser/src/app/words/adverbs.csv", "r") as file:
         rows = csv.reader(file)
         for row in rows:
             new = Adverb.Adverb(word=row)
-            db.session.add(new)
-        db.session.commit()
+            local_session.add(new)
+
+        local_session.commit()
 
 def populate_nouns():
     with open("/home/projuser/src/app/words/nouns.csv", "r") as file:
         rows = csv.reader(file)
         for row in rows:
             new = Noun.Noun(word=row)
-            db.session.add(new)
-        db.session.commit()
+            local_session.add(new)
+
+        local_session.commit()
 
 def populate_verbs():
     with open("/home/projuser/src/app/words/verbs.csv", "r") as file:
         rows = csv.reader(file)
         for row in rows:
             new = Verb.Verb(word=row)
-            db.session.add(new)
-        db.session.commit()
+            local_session.add(new)
+
+        local_session.commit()
 
 def main():
     populate_adjectives()
